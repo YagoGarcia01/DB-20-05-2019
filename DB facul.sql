3c8a035af8adb857d96aaed6fd0f8b5cc744305e
YagoGarcia01 <37703170+YagoGarcia01@users.noreply.github.com>
2019-06-10 20:23:53 -0300

    DB-Facul

---
+++ DB facul.sql
@@ -0,0 +1,17 @@
+create database Faculdade;
+use Faculdade;
+create table tbInstrutores(id int(10) primary key auto_increment not null, nome varchar(50), email varchar(50), valor_hora int(10), certificados varchar(255));
+create table tbCursos(id int(10) primary key auto_increment not null, nome varchar(50), requisito varchar(255), carga_horaria smallint(5), preco double);
+create table tbAlunos(id int(11) primary key auto_increment not null, cpf char(11), nome varchar(50), email varchar(50), fone char(14), data_nascimento date);
+create table tbTurmas(id int(10) primary key auto_increment not null, data_inicio date, data_final date, carga_horaria smallint(5));
+alter table tbTurmas add column instrutores_id int(11);
+alter table tbTurmas add column cursos_id int(10);
+alter table tbTurmas add constraint fkturmas_FKIndex1 foreign key (instrutores_id) references tbInstrutores (id);
+create table tbMatriculas(id int(10) primary key auto_increment not null, turmas_id int(10), alunos_id int(11), data_matricula date);
+alter table tbTurmas add constraint fkturmas_FKIndex2 foreign key (cursos_id) references tbCursos (id);
+alter table tbMatriculas add constraint fkmatriculas_FKIndex1 foreign key (alunos_id) references tbAlunos (id);
+alter table tbMatriculas add constraint fkmatriculas_FKIndex3 foreign key (turmas_id) references tbTurmas (id);
+insert into tbInstrutores (nome,email,valor_hora,certificados) values("Alexandre", "Alexandre_cobit@outlook.com", 2, "Cobit");
+insert into tbInstrutores (nome,email,valor_hora,certificados) values("Alex", "Alex_scrum@outlook.com", 3, "scrum");
+insert into tbInstrutores (nome,email,valor_hora,certificados) values("Sergio", "Sergio_cobit@outlook.com", 4, "Cobit");
+insert into tbInstrutores (nome,email,valor_hora,certificados) values("Giovanni", "Giovanni_Itil@outlook.com", 5, "Itil");
